9a4da88ff277263f8d5b4acb9f1c1230e771b3d3
vtarasiuk <72463801+vtarasiuk@users.noreply.github.com>
2021-07-24 14:18:34 +0300

    Create utils.h

---
+++ Lab_6/utils.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <Windows.h>
+
+int** createMatrix(int n, int m);
+int* createArray(int n);
+int** copyMatrix(int** base, int n, int m);
+int** createI(int n);
+void pause(HWND* hWnd);
